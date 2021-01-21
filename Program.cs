69bc27575a41a077a9d8a276f11e20b98f2d39da
Joseph M. Myers <joecaricature@gmail.com>
2021-01-21 18:26:07 -0500

    Creating the Deck

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BlackJack
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Deck
     {
-      Console.WriteLine("Welcome to C#");
+
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var suits = new List<string>() { "Clubs", "Hearts", "Spades", "Diamonds" };
+            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            var deck = new List<string>();
+
+
+        }
+    }
 }
