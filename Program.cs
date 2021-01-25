9afe36aa9fbcc64691a0323e43da675ca2b4f24a
Joseph M. Myers <joecaricature@gmail.com>
2021-01-25 17:58:05 -0500

    Building Card Class

--- Program.cs
+++ Program.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 
 namespace BlackJack
 {
-    class Deck
+    public class Card
+    {
+        public string suit;
+        public string rank;
+        // now I need the card constructor, but I can't remember how to pull this off
+        public cards
+
+    }
+    public class Deck
     {
 
     }
@@ -11,9 +19,9 @@ namespace BlackJack
     {
         static void Main(string[] args)
         {
-            var suits = new List<string>() { "Clubs", "Hearts", "Spades", "Diamonds" };
-            var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
-            var deck = new List<string>();
+            // var suits = new List<string>() { "Clubs", "Hearts", "Spades", "Diamonds" };
+            // var ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            // var deck = new List<string>();
 
 
         }
